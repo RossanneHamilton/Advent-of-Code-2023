f6d094349bbf8f0d025ba3f82c59dfb1679c6913
Lithill <rossanneh@gmail.com>
2023-12-03 10:14:09 +0000

    Add symbol and position to array

--- day3/q1.js
+++ day3/q1.js
@@ -8,19 +8,16 @@ let input = "467..114.. ..35..633. 617*...... .....+.58. ..592..... ......755. .
 let symbolPosition = [];
 let inputArr = input.split(" ")
 
-console.log(inputArr)
-
 function findSymbolPosition(arr) {
     let lineNo;
-    let positionNo;
 
     for (let i = 0; i < arr.length; i++) {
         lineNo = i+1;
-        console.log("line number: " + lineNo)
 
-        for (let i = 0; i < arr.length; i++) {
-            positionNo = i+1;
-            console.log("positionNo: " + positionNo)
+        for (let j = 0; j < arr[i].length; j++) {
+            let positionNo = j+1;
+            let currentChar = arr[i][j];
+            symbolPosition.push([currentChar, lineNo, positionNo]);
         };
     };
 }
